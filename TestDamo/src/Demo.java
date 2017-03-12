d31781f5559515738ad8a623c687964d964ab181
Administrator <Administrator@WIN8-1612142013>
2017-03-12 16:29:23 +0800

    1

--- TestDamo/src/Demo.java
+++ TestDamo/src/Demo.java
@@ -3,8 +3,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("out");
-        System.out.println("out2");
+        System.out.println("ou2t");
+        System.out.println("out22");
 	}
 
 }
